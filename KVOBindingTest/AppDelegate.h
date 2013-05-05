756df5b2e886ba0aeaf6b96d0f4fb8473ffd7147
Daniel Tull <dt@danieltull.co.uk>
2013-05-05 15:57:59 +0100

    Initial Commit

---
+++ KVOBindingTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KVOBindingTest
+//
+//  Created by Daniel Tull on 05.05.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
